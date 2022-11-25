614cad41a8be887f5706d02ab07cbee870df03d4
yoavgilad04 <yoavgilad1995g@gmail.com>
2022-11-25 12:57:47 +0200

    somthing

---
+++ skeleton_smash/cmake-build-debug/CMakeFiles/skeleton_smash.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skeleton_smash.dir/smash.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/Commands.cpp.o"
+  "CMakeFiles/skeleton_smash.dir/signals.cpp.o"
+  "skeleton_smash.pdb"
+  "skeleton_smash"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skeleton_smash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
