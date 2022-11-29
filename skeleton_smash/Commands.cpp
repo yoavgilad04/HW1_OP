7f9be86c53004aaecdc55d340d06bc5d93e1e230
Shai Tayar <47159139+shaitayar@users.noreply.github.com>
2022-11-29 18:34:15 +0200

    running tests

--- skeleton_smash/Commands.cpp
+++ skeleton_smash/Commands.cpp
@@ -183,7 +183,7 @@ void Chprompt::execute() { //TODO: ADD ERRORS
     int num_args = _parseCommandLine(this->getCommandLine(), args);
     if (num_args == 1) {
         shell.ChangePrompt("");
-    } else if (num_args == 2) {
+    } else {
         shell.ChangePrompt(args[1]);
     }
 }
@@ -262,16 +262,15 @@ void ChangeDirCommand::execute() {
 void QuitCommand::execute() {
     const char * cmd_c = this->getCommandLine();
     string cmd_s = _trim(string(cmd_c));
-
-    if(cmd_s.find("kill") != std::string::npos){
-        cout<<"smash: sending SIGKILL signal to " << jobs->getCurrJobsNum()<< " jobs:"<<endl;
+    char * args[COMMAND_ARGS_MAX_LENGTH];
+    _parseCommandLine(this->getCommandLine(), args);
+    if(string(args[1]).compare("kill")==0){
+        cout<<"smash: sending SIGKILL signal to " << jobs->getCurrJobsNum()<< " jobs: "<<endl;
         jobs->printJobsList();
-        jobs->killAllJobs();
     }
-    else{
-        jobs->killAllJobs();
-    }
-    return;
+    jobs->killAllJobs();
+    delete this;
+    exit(0);
 }
 
 /***ForegroundCommand implementation
@@ -378,7 +377,7 @@ void BackgroundCommand::execute() {
     }
 
     pid_t pid = job_to_bg->getJobPid();
-    cout << job_to_bg->getCmd()->getCommandLine() << ' : ' << pid << endl;
+    cout << job_to_bg->getCmd()->getCommandLine() << " : " << pid << endl;
 
     //resume in background
     if (kill(pid, SIGCONT) == SYS_FAIL) {
@@ -453,7 +452,7 @@ void KillCommand::execute() {
             break;
             //todo: add more cases and think how to act for each sig
     }
-
+    cout<< "signal number "<<signal_num<<" was sent to pid "<<job->getJobPid()<<endl;
 
 }
 
@@ -540,6 +539,7 @@ void JobsList::addJob(Command *cmd, pid_t job_pid, bool isStopped) {
 
 /***printJobsList- this function prints all the jobs that currently in JobList */
 void JobsList::printJobsList() {
+    if(this->max_job_id==0) return;
     removeFinishedJobs();
 
     for (auto it = jobs_vect.begin(); it != jobs_vect.end(); ++it) {
@@ -562,13 +562,10 @@ void JobsList::printJobsList() {
 
 /***killAllJobs- this function kills all the jobs that currently in JobList */
 void JobsList::killAllJobs() {
-    removeFinishedJobs();
     for (auto it = jobs_vect.begin(); it != jobs_vect.end(); ++it) {
-        cout << "smash: process " << (*it)->getJobPid() << " was killed" << endl;
+        cout << (*it)->getJobPid() << ": " << (*it)->getCmd()->getCommand() << endl;
+        //cout << "smash: process " << (*it)->getJobPid() << " was killed" << endl;
         kill((*it)->getJobPid(), SIGKILL);
-        delete (*it)->getCmd(); //check if OK
-        jobs_vect.erase(it);
-        it--;
     }
 }
 
@@ -709,6 +706,8 @@ void ExternalCommand::execute() {
             this->err.PrintSysFailError("fork");
             return;
         }
+        shell.setFgPID(child_pid);
+        shell.setFgCmd(this);
         if (pid == 0) {
             if (setpgrp() == SYS_FAIL) {
                 this->err.PrintSysFailError("setpgrp");
@@ -719,15 +718,16 @@ void ExternalCommand::execute() {
                 return;
             } else { // pid != 0 Parent code
                 child_pid = pid;
-                if (!is_background) {
-                    shell.setFgPID(&child_pid);
-                    shell.setFgCmd(this);
+                if (is_background) {
+                    shell.GetJobList()->addJob(this, child_pid, false);
+                }
+                else{
                     if (waitpid(child_pid, nullptr, WUNTRACED) == SYS_FAIL) {
                         this->err.PrintSysFailError("waitpid");
                         return;
-                    } else { //in background
-                        shell.GetJobList()->addJob(this, child_pid, false);
                     }
+                    shell.setFgPID(-1);
+                    shell.setFgCmd(nullptr);
                 }
             }
         }
@@ -738,7 +738,10 @@ void ExternalCommand::execute() {
         if (pid == SYS_FAIL) {
             perror("fork");
             return;
-        } else if (pid == 0) { //son
+        }
+        shell.setFgPID(pid);
+        shell.setFgCmd(this);
+        if (pid == 0) { //son
             if (setpgrp() == SYS_FAIL) {
                 this->err.PrintSysFailError("setpgrp");
                 return;
@@ -751,11 +754,12 @@ void ExternalCommand::execute() {
             if (is_background) {
                 shell.GetJobList()->addJob(this, pid, false);
             } else {
-                shell.setFgPID(&pid);
-                shell.setFgCmd(this);
+
                 if (waitpid(pid, nullptr, WUNTRACED) == SYS_FAIL) {
                     this->err.PrintSysFailError("waitpid");
                 }
+                shell.setFgCmd(nullptr);
+                shell.setFgPID(-1);
             }
         }
     }
@@ -806,9 +810,14 @@ void PipeCommand::execute() {
         return;
     }
     if (c1_pid == 0){
+        if (setpgrp() == SYS_FAIL) {
+            this->err.PrintSysFailError("setpgrp");
+            this->closePipe(fd);
+            return;
+        }
         int d_res;
         if (is_error)
-           d_res = dup2(fd[1], 3);
+           d_res = dup2(fd[1], 2);
         else
             d_res = dup2(fd[1], 1);
         if (d_res == SYS_FAIL){
@@ -818,9 +827,21 @@ void PipeCommand::execute() {
         }
         this->closePipe(fd);
         shell.executeCommand(this->command1);
+        exit(0);
     }
     pid_t c2_pid = fork();
+    if (c2_pid == SYS_FAIL){
+        this->err.PrintSysFailError("fork");
+        this->closePipe(fd);
+        return;
+    }
+
     if (c2_pid == 0){
+        if (setpgrp() == SYS_FAIL) {
+            this->err.PrintSysFailError("setpgrp");
+            this->closePipe(fd);
+            return;
+        }
         int d_res = dup2(fd[0], 0);
         if (d_res == SYS_FAIL){
             this->err.PrintSysFailError("dup2");
@@ -829,8 +850,21 @@ void PipeCommand::execute() {
         }
         this->closePipe(fd);
         shell.executeCommand(this->command2);
+        exit(0);
     }
     this->closePipe(fd);
+    if(waitpid(c1_pid, nullptr, WUNTRACED)==SYS_FAIL){
+        this->err.PrintSysFailError("waitpid");
+        return;
+    }
+    if(waitpid(c1_pid, nullptr, WUNTRACED)==SYS_FAIL){
+        this->err.PrintSysFailError("waitpid");
+        return;
+    }
+    if(waitpid(c2_pid, nullptr, WUNTRACED)==SYS_FAIL){
+        this->err.PrintSysFailError("waitpid");
+        return;
+    }
 }
 
 /***--------------Redirection Command implementation--------------***/
@@ -928,13 +962,12 @@ void SmallShell::ChangePrompt(string new_prompt) {
 SmallShell::SmallShell() {
     this->jobs_list = new JobsList();
     this->p_last_dir = nullptr;
-    this->fg_pid = new pid_t ;
+    this->fg_pid = -1;
     this->fg_cmd = nullptr;
 }
 
 SmallShell::~SmallShell() {
     delete jobs_list;
-    delete fg_pid;
 }
 
 /**
@@ -947,11 +980,11 @@ Command *SmallShell::CreateCommand(const char *cmd_line) {
     if (firstWord.back() == '&') {
         firstWord.pop_back();
     }
-    if(cmd_s.find_first_of(">>") != std::string::npos || cmd_s.find_first_of('>') != std::string::npos){
+    if(cmd_s.find(">>") != std::string::npos || cmd_s.find('>') != std::string::npos){
         return new RedirectionCommand(cmd_line);
     }
 
-    if(cmd_s.find_first_of("|&") != std::string::npos || cmd_s.find_first_of('|') != std::string::npos){
+    if(cmd_s.find("|&") != std::string::npos || cmd_s.find('|') != std::string::npos){
         return new PipeCommand(cmd_line);
     }
 
@@ -970,7 +1003,7 @@ Command *SmallShell::CreateCommand(const char *cmd_line) {
     } else if (firstWord.compare("bg") == 0) {
         return new BackgroundCommand(cmd_line, jobs_list);
     } else if (firstWord.compare("quit") == 0) {
-        //return new QuitCommand(cmd_line, jobs_list);
+        return new QuitCommand(cmd_line, jobs_list);
     } else if (firstWord.compare("kill") == 0) {
         //return new KillCommand(cmd_line, jobs_list);
     } else if (firstWord.compare("fare") == 0) {
@@ -978,7 +1011,7 @@ Command *SmallShell::CreateCommand(const char *cmd_line) {
     } else if (firstWord.compare("setcore") == 0) {
         return new SetCoreCommand(cmd_line, jobs_list);
     } else {
-        return new ExternalCommand(cmd_line, this->fg_pid);
+        return new ExternalCommand(cmd_line);
     }
 
     return nullptr;
@@ -991,4 +1024,5 @@ void SmallShell::executeCommand(const char *cmd_line) {
         return;
     }
     cmd->execute();
+    //delete cmd;
 }
--- skeleton_smash/signals.cpp
+++ skeleton_smash/signals.cpp
@@ -6,12 +6,11 @@ using namespace std;
 
 void ctrlZHandler(int sig_num) {
     SmallShell &shell = SmallShell::getInstance();
-    if (*(shell.getFgPID()) == -1 | shell.getFgCmd() == nullptr){
-        cout<<"nothing should happen"<<endl;
+    if (shell.getFgPID() == -1 || shell.getFgCmd() == nullptr){
         return;
     }
     cout << "smash: got ctrl-Z" << endl ;
-    pid_t fg_pid = *(shell.getFgPID());
+    pid_t fg_pid = shell.getFgPID();
     if (kill(fg_pid, SIGSTOP) == -1){
         perror("smash error: kill failed");
         return;
@@ -19,26 +18,25 @@ void ctrlZHandler(int sig_num) {
     cout<< "smash: process "<< fg_pid << " was stopped" <<endl;
     shell.GetJobList()->addJob(shell.getFgCmd(),fg_pid,true);
     shell.setFgCmd(nullptr);
-    shell.setFgPID(nullptr);
+    shell.setFgPID(-1);
     return;
 }
 
 void ctrlCHandler(int sig_num) {
     SmallShell &shell = SmallShell::getInstance();
-    if (*(shell.getFgPID()) == -1 | shell.getFgCmd() == nullptr){
-        cout<<"nothing should happen"<<endl;
+    if (shell.getFgPID() == -1 || shell.getFgCmd() == nullptr){
         return;
     }
     else{
         cout << "smash: got ctrl-C" << endl ;
-        pid_t fg_pid = *(shell.getFgPID());
+        pid_t fg_pid = shell.getFgPID();
         if (kill(fg_pid, SIGINT) == -1){
             perror("smash error: kill failed");
             return;
         }
         cout<< "smash: process "<< fg_pid << " was killed" <<endl;
         shell.setFgCmd(nullptr);
-        shell.setFgPID(nullptr);
+        shell.setFgPID(-1);
     }
 
 }
